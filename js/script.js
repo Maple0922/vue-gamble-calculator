c2086f6972a79560e149ad78c255f9b171e31b15
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-06-14 07:53:18 +0900

    2020/06/14 07:53

--- js/script.js
+++ js/script.js
@@ -7,12 +7,18 @@ var main = new Vue({
       { name: '辰野', selected: false },
       { name: '中島', selected: false }
     ],
-    games: ['大富豪','麻雀'],
+    games: [
+      { name: '大富豪', rate: 300, selected: true},
+      { name: '麻雀', rate: 30, selected: false},
+      { name: 'みんはや', rate: 300, selected: false},
+    ],
+    rateDef: 300,
+    rateStep: 100,
     userCount: 0,
     pageCount: 0,
-    games: [
-      { name: '大富豪', rate: 300 }
-    ]
+    newGameMaker: false,
+    newButtonText: '+',
+    gameTypes: []
   },
   methods: {
     select: function(e){
@@ -31,10 +37,26 @@ var main = new Vue({
     back: function(){
       this.pageCount--;
     },
-    newgame: function(){
+    newGame: function(){
+      this.newButtonText = this.newGameMaker ? '＋' : '×';
+      this.newGameMaker = !this.newGameMaker;
+    },
+    newCalc: function(){
       this.games.push(
         { name: '麻雀', rate: 30 }
       );
+    },
+    gameSelect: function(e){
+        const listItemsNode = document.querySelectorAll('.new-game-maker li');
+        const listItems = [].slice.call(listItemsNode);
+        const clickedItem = e.target;
+        const index = listItems.indexOf(clickedItem);
+        for (var i = 0; i < this.games.length; i++) {
+          this.games[i].selected = false;
+        }
+        this.games[index].selected = !this.games[index].selected;
+        this.rateDef = this.games[index].rate;
+        this.rateStep = this.rateDef/3;
+      }
     }
-  }
-});
+  });
