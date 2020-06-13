0f2391313c0c7cb1c49d18b23feefe80d9dc6555
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-06-14 02:52:21 +0900

    2020/06/14 02:52

--- js/script.js
+++ js/script.js
@@ -14,14 +14,15 @@ var main = new Vue({
     select: function(e){
       name = e.target.innerText;
       for (var i = 0; i < this.users.length; i++) {
-        // let item = this.users[i];
-        // if (item.name == name) {
-        //   if (item.selected) {
-        //     item.selected = false;
-        //   }else{
-        //     item.selected = true;
-        //   }
-        // }
+        let item = this.users[i];
+        if (item.name == name) {
+          if (item.selected) {
+            item.selected = false;
+          }else{
+            item.selected = true;
+          }
+        }
+
         console.log(this.users[i].name);
         console.log(this.users[i].selected);
       }
