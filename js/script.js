e87f370b3da3b25eff9918cdd11c9e072c96f00f
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-06-14 02:52:10 +0900

    2020/06/14 02:52

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 var main = new Vue({
   el: '.main',
   data: {
-    message: 'test'
+    users: [
+      { name: '黒田', selected: false },
+      { name: '佐藤', selected: false },
+      { name: '辰野', selected: false },
+      { name: '中島', selected: false }
+    ],
+    games: ['大富豪','麻雀'],
+    userCount: 0
+  },
+  methods: {
+    select: function(e){
+      name = e.target.innerText;
+      for (var i = 0; i < this.users.length; i++) {
+        // let item = this.users[i];
+        // if (item.name == name) {
+        //   if (item.selected) {
+        //     item.selected = false;
+        //   }else{
+        //     item.selected = true;
+        //   }
+        // }
+        console.log(this.users[i].name);
+        console.log(this.users[i].selected);
+      }
+    }
   }
 });
